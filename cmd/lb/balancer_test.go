197c3dc13f0c9ad3503120e37d5dff3fcd1b35b1
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-05-25 16:42:49 +0200

    Implement `balancer_test`

--- cmd/lb/balancer_test.go
+++ cmd/lb/balancer_test.go
@@ -1,7 +1,66 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+	"time"
+)
+
+type FakeAlwaysTrueHealthChecker struct{}
+
+func (hc *FakeAlwaysTrueHealthChecker) Check(server string) bool {
+	return true
+}
+
+type FakeReturnRequestBodyRequestSender struct{}
+
+func (rs *FakeReturnRequestBodyRequestSender) Send(request *http.Request) (*http.Response, error) {
+	bodyBytes, err := ioutil.ReadAll(request.Body)
+	if err != nil {
+		return nil, err
+	}
+	return &http.Response{
+		StatusCode: 200,
+		Proto:      "HTTP/1.1",
+		Header:     make(http.Header),
+		Body:       ioutil.NopCloser(bytes.NewBuffer(bodyBytes)),
+		Request:    &http.Request{},
+		Close:      false,
+	}, nil
+}
+
+func TestScheme(t *testing.T) {
+	*https = true
+	assert.Equal(t, "https", scheme(), "Expected scheme to be https")
+
+	*https = false
+	assert.Equal(t, "http", scheme(), "Expected scheme to be http")
+}
 
 func TestBalancer(t *testing.T) {
-	// TODO: Реалізуйте юніт-тест для балансувальникка.
+	healthChecker = &FakeAlwaysTrueHealthChecker{}
+	requestSender = &FakeReturnRequestBodyRequestSender{}
+	serversPool = []string{"http://server1:1", "http://server2:1", "http://server3:1"}
+	healthCheck(serversPool)
+	time.Sleep(10 * time.Second)
+
+	server := chooseServer()
+	fmt.Println(server)
+	assert.NotNil(t, server, "Assert server is not nil")
+	assert.Contains(t, server, "http://server", "Assert server is not nil")
+
+	err := forward("http://server1:1", httptest.NewRecorder(), httptest.NewRequest("GET", "http://server1:1", strings.NewReader("body length 14")))
+	assert.NoError(t, err)
+	err = forward("http://server3:1", httptest.NewRecorder(), httptest.NewRequest("GET", "http://server3:1", strings.NewReader("body length 14")))
+	assert.NoError(t, err)
+
+	server = chooseServer()
+
+	assert.Equal(t, "http://server2:1", server, "Expected server2 to be chosen")
 }

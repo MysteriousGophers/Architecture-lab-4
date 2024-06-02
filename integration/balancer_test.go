b6bc20347fc46c7c5b2e315928444b2d25133637
Stanislav Basarab <stanislavbasarab083@gmail.com>
2024-06-02 22:19:47 +0300

    Implement integration test for checking database

--- integration/balancer_test.go
+++ integration/balancer_test.go
@@ -1,6 +1,7 @@
 package integration
 
 import (
+	"encoding/json"
 	"fmt"
 	. "gopkg.in/check.v1"
 	"net/http"
@@ -16,6 +17,12 @@ type IntegrationSuite struct{}
 var _ = Suite(&IntegrationSuite{})
 
 const baseAddress = "http://balancer:8090"
+const team = "MysteriousGophers"
+
+type Response struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+}
 
 var client = http.Client{
 	Timeout: 3 * time.Second,
@@ -26,7 +33,7 @@ func (s *IntegrationSuite) TestBalancer(c *C) {
 		c.Skip("Integration test is not enabled")
 	}
 
-	server1, err := client.Get(fmt.Sprintf("%s/check", baseAddress))
+	server1, err := client.Get(fmt.Sprintf("%s/some", baseAddress))
 	if err != nil {
 		c.Error(err)
 	}
@@ -34,7 +41,7 @@ func (s *IntegrationSuite) TestBalancer(c *C) {
 	server1Header := server1.Header.Get("lb-from")
 	c.Check(server1Header, Equals, "server1:8080")
 
-	server2, err := client.Get(fmt.Sprintf("%s/check4", baseAddress))
+	server2, err := client.Get(fmt.Sprintf("%s/some1", baseAddress))
 	if err != nil {
 		c.Error(err)
 	}
@@ -42,7 +49,7 @@ func (s *IntegrationSuite) TestBalancer(c *C) {
 	server2Header := server2.Header.Get("lb-from")
 	c.Check(server2Header, Equals, "server2:8080")
 
-	server3, err := client.Get(fmt.Sprintf("%s/check2", baseAddress))
+	server3, err := client.Get(fmt.Sprintf("%s/some2", baseAddress))
 	if err != nil {
 		c.Error(err)
 	}
@@ -50,13 +57,31 @@ func (s *IntegrationSuite) TestBalancer(c *C) {
 	server3Header := server3.Header.Get("lb-from")
 	c.Check(server3Header, Equals, "server3:8080")
 
-	server1Repeat, err := client.Get(fmt.Sprintf("%s/check", baseAddress))
+	server1Repeat, err := client.Get(fmt.Sprintf("%s/some1", baseAddress))
 	if err != nil {
 		c.Error(err)
 	}
 
 	server1RepeatHeader := server1Repeat.Header.Get("lb-from")
 	c.Check(server1RepeatHeader, Equals, server1Header)
+
+	// check database
+	db, err := client.Get(fmt.Sprintf("%s/api/v1/some-data?key=MysteriousGophers", baseAddress))
+
+	if err != nil {
+		c.Error(err)
+	}
+
+	var body Response
+	err = json.NewDecoder(db.Body).Decode(&body)
+	if err != nil {
+		c.Error(err)
+	}
+
+	c.Check(body.Key, Equals, team)
+	if body.Value == "" {
+		c.Error(err)
+	}
 }
 
 func (s *IntegrationSuite) BenchmarkBalancer(c *C) {
